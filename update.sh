3d86a4fd1cb83506531989bf64820aa2e12c27cf
i-b8o <blackview1617@gmail.com>
2022-11-12 12:21:07 +0300

    again

--- update.sh
+++ update.sh
@@ -6,11 +6,16 @@ function_get() {
 read -p "Enter a tag: " tag
 cd ../regulations_writable_service/app
 function_get $tag "writable"
+pwd
 cd ../regulations_read_only_service/app
 function_get $tag "read only"
+pwd
 cd ../regulations_supreme_service/app
 function_get $tag "supreme"
+pwd
 cd ../regulations_mobile_service/app
-function_get $tag "mobilt"
+function_get $tag "mobile"
+pwd
 cd ../regulations_parser/app
 function_get $tag "parser"
+pwd
