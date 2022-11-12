8e2a9e9aaeb4722215a7efe84b5e71d207b7e700
i-b8o <blackview1617@gmail.com>
2022-11-12 12:22:14 +0300

    again

--- update.sh
+++ update.sh
@@ -7,15 +7,15 @@ read -p "Enter a tag: " tag
 cd ../regulations_writable_service/app
 function_get $tag "writable"
 pwd
-cd ../regulations_read_only_service/app
+cd ../../regulations_read_only_service/app
 function_get $tag "read only"
 pwd
-cd ../regulations_supreme_service/app
+cd ../../regulations_supreme_service/app
 function_get $tag "supreme"
 pwd
-cd ../regulations_mobile_service/app
+cd ../../regulations_mobile_service/app
 function_get $tag "mobile"
 pwd
-cd ../regulations_parser/app
+cd ../../regulations_parser/app
 function_get $tag "parser"
 pwd
