88ceecd88a94d9d8de286c6ac880e9a2bb5400f2
i-b8o <blackview1617@gmail.com>
2022-11-12 12:17:10 +0300

    again

--- update.sh
+++ update.sh
@@ -2,15 +2,14 @@
 function_get() {
     go get -u github.com/i-b8o/regulations_contracts@$1
 }
-echo 'tag:'
-read tag
+read -p "Enter a tag: " tag
 cd ../regulations_writable_service/app
-function_get tag
+function_get $tag
 cd ../../regulations_read_only_service/app
-function_get tag
+function_get $tag
 cd ../../regulations_supreme_service/app
-function_get tag
+function_get $tag
 cd ../../regulations_mobile_service/app
-function_get tag
+function_get $tag
 cd ../../regulations_parser/app
-function_get tag
+function_get $tag
