882096d582c9d3dd6f08aa926299e8196ca91bdf
i-b8o <blackview1617@gmail.com>
2022-11-15 10:44:56 +0300

    replaced regulations with read-only

--- update.sh
+++ update.sh
@@ -4,17 +4,17 @@ function_get() {
     go get -u github.com/i-b8o/read-only_contracts@$1
 }
 read -p "Enter a tag: " tag
-cd ../read-only_writer_service/app
+cd ../read-only_writer_service
 function_get $tag "writer"
-cd ../../read-only_reader_service/app
+cd ../../read-only_reader_service
 function_get $tag "reader"
-cd ../../read-only_master_service/app
+cd ../../read-only_master_service
 function_get $tag "master"
-# cd ../../read-only_searcher_service/app
+# cd ../../read-only_searcher_service
 # function_get $tag "searcher"
-cd ../../read-only_mobile_service/app
+cd ../../read-only_mobile_service
 function_get $tag "mobile"
-cd ../../read-only_http_server/app
+cd ../../read-only_http_server
 function_get $tag "http"
-cd ../../read-only_parser/app
+cd ../../read-only_parser
 function_get $tag "parser"
