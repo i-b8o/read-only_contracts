e0c5ea6638040134046c0a900b1e5f96d19e127c
i-b8o <blackview1617@gmail.com>
2022-12-26 22:29:04 +0300

    added

--- sql/init.sql
+++ sql/init.sql
@@ -1,12 +1,23 @@
 DROP TABLE IF EXISTS public.paragraph;
 DROP TABLE IF EXISTS public.chapter;
 DROP TABLE IF EXISTS public.doc;
+DROP TABLE IF EXISTS public.subtype;
+DROP TABLE IF EXISTS public.type;
 
+CREATE TABLE public.type (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL CHECK (NAME != '') UNIQUE
+);
+CREATE TABLE public.subtype (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL CHECK (NAME != '') UNIQUE,
+    type_id integer REFERENCES type
+);
+CREATE INDEX subtype_type_idx ON public.subtype (type_id);
 CREATE TABLE public.doc (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL CHECK (NAME != '') UNIQUE,
-    type TEXT NOT NULL,
-    subtype TEXT NOT NULL,
+    subtype_id integer REFERENCES subtype,
     rev TEXT,
     title TEXT DEFAULT ' ' NOT NULL,
     description TEXT DEFAULT ' ' NOT NULL,
@@ -14,6 +25,7 @@ CREATE TABLE public.doc (
     created_at TIMESTAMPTZ NOT NULL,
     updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
+CREATE INDEX doc_subtype_idx ON public.doc (subtype_id);
 CREATE TABLE public.chapter (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL CHECK (name != ''),
@@ -40,12 +52,16 @@ CREATE TABLE public.paragraph (
 CREATE INDEX paragraph_chaptedoc_idx ON public.paragraph (c_id);
 CREATE USER reader WITH ENCRYPTED PASSWORD '031501';
 GRANT CONNECT ON DATABASE main TO reader;
+GRANT SELECT ON TABLE public.type TO reader;
+GRANT SELECT ON TABLE public.subtype TO reader;
 GRANT SELECT ON TABLE public.doc TO reader;
 GRANT SELECT ON TABLE public.chapter TO reader;
 GRANT SELECT ON TABLE public.paragraph TO reader;
 CREATE USER writer WITH ENCRYPTED PASSWORD '031501';
 GRANT CONNECT ON DATABASE main TO writer;
 GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO writer;
+GRANT ALL PRIVILEGES ON TABLE public.type TO writer;
+GRANT ALL PRIVILEGES ON TABLE public.subtype TO writer;
 GRANT ALL PRIVILEGES ON TABLE public.doc TO writer;
 GRANT ALL PRIVILEGES ON TABLE public.chapter TO writer;
 GRANT ALL PRIVILEGES ON TABLE public.paragraph TO writer;
